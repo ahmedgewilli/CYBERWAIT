9c90e3c5e1b1537db159e40136381707c1b2d18b
ahmedgewilli <gewilliahmed@gmail.com>
2026-01-08 11:27:39 +0200

    fix: rename supabaseServer and update order table schema

--- api/orders.js
+++ api/orders.js
@@ -1,4 +1,4 @@
-import { createSupabaseServerClient } from './_supabaseServer';
+import { createSupabaseServerClient } from './supabaseServer';
 
 export default async function handler(req, res) {
   if (req.method === 'OPTIONS') return res.status(200).end();
